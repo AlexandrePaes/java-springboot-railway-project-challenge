c66787439fe8ed6d133e10d5a4c6b97885110918
kalinux <kalinux@kalinux.kalinux.com>
2024-08-13 21:49:23 -0400

    Inicial commit to upload and visualize the Readme.md file

---
+++ mob-banking/src/main/java/com/san/MobBankingApplication.java
@@ -0,0 +1,11 @@
+package com.san;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobBankingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MobBankingApplication.class, args);
+	}
+}
